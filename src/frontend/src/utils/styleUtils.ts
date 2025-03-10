c51078bfbf1e2a81f12ce87868fda5bcfafbb4c7
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2025-03-10 09:45:56 -0300

    feat: Add JSON node color to styleUtils

--- src/frontend/src/utils/styleUtils.ts
+++ src/frontend/src/utils/styleUtils.ts
@@ -482,6 +482,7 @@ export const nodeColorsName: { [char: string]: string } = {
   unknown: "gray",
   Document: "lime",
   Data: "red",
+  JSON: "red",
   Message: "indigo",
   Prompt: "violet",
   Embeddings: "emerald",
